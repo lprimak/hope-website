802aedb0691c072176e67c51d7e58ad17626bf55
lprimak <lprimak@localhost>
2011-04-01 17:39:15 -0400

    first stab at buttons

--- src/java/com/flowlogix/website/pages/EraseJunkMail.java
+++ src/java/com/flowlogix/website/pages/EraseJunkMail.java
@@ -2,7 +2,28 @@ package com.flowlogix.website.pages;
 
 import lombok.Getter;
 
+import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.annotations.Persist;
+
 public class EraseJunkMail
 {
-    @Getter private final String hello = "Lenny Primak"; 
+    @SuppressWarnings("unused")
+    @OnEvent(component="eraseJunkMail", value="clicked")
+    private void eraseJunkMail()
+    {
+        junkMailErased = "yes!";
+    }
+
+    
+    @SuppressWarnings("unused")
+    @OnEvent(component="reset", value="clicked")
+    private void resetJunkMail()
+    {
+        junkMailErased = "no!";
+    }
+
+    
+    
+    @Persist
+    @Getter private String junkMailErased;
 }
