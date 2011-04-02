9d6fce22a07fffe999d0a1ad521fc65292553c9f
lprimak <lprimak@localhost>
2011-04-02 13:00:22 -0500

    global improvements

---
+++ src/java/com/flowlogix/website/services/HopeModule.java
@@ -0,0 +1,23 @@
+package com.flowlogix.website.services;
+
+import org.apache.tapestry5.SymbolConstants;
+import org.apache.tapestry5.ioc.MappedConfiguration;
+import org.apache.tapestry5.ioc.ServiceBinder;
+
+public class HopeModule 
+{
+    public static void bind(ServiceBinder binder)
+    {
+        // binder.bind(MyServiceInterface.class, MyServiceImpl.class);
+        
+        // Make bind() calls on the binder object to define most IoC services.
+        // Use service builder methods (example below) when the implementation
+        // is provided inline, or requires more initialization than simply
+        // invoking the constructor.
+    }
+    
+	public static void contributeApplicationDefaults(MappedConfiguration<String, String> configuration)
+	{
+		configuration.add(SymbolConstants.PRODUCTION_MODE, "false");
+	}
+}
