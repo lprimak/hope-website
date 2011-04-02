9906afcea7843ac87b4390423aaeb37771df9434
lprimak <lprimak@localhost>
2011-04-02 16:07:50 -0500

    working

---
+++ src/java/com/flowlogix/website/impl/JunkMailEraser.java
@@ -0,0 +1,38 @@
+package com.flowlogix.website.impl;
+
+import java.util.logging.Logger;
+
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.mail.Flags.Flag;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Store;
+
+import lombok.Cleanup;
+import lombok.SneakyThrows;
+
+@Stateless
+public class JunkMailEraser 
+{
+	@SneakyThrows({ MessagingException.class })
+	public void erase()
+	{
+        @Cleanup Store store = mailSession.getStore();
+        log.fine(mailSession.getProperties().toString());
+        store.connect(mailSession.getProperty("mail.user"), mailSession.getProperty("mail.password"));
+        Folder junkFolder = store.getFolder("Junk");
+        junkFolder.open(Folder.READ_WRITE);
+        Message[] messages = junkFolder.getMessages();
+        for(Message msg : messages)
+        {
+        	msg.setFlag(Flag.DELETED, true);
+        }
+        junkFolder.close(true);        
+	}
+	
+	@Resource(name="mail/HopeMail") private Session mailSession;
+	private Logger log = Logger.getLogger(getClass().getName());
+}
--- src/java/com/flowlogix/website/pages/EraseJunkMail.java
+++ src/java/com/flowlogix/website/pages/EraseJunkMail.java
@@ -1,17 +1,30 @@
 package com.flowlogix.website.pages;
 
-import lombok.Getter;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import lombok.SneakyThrows;
 
 import org.apache.tapestry5.annotations.AfterRender;
 import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Persist;
 
+import com.flowlogix.website.impl.JunkMailEraser;
+
 public class EraseJunkMail
 {
+	@SneakyThrows(NamingException.class)
+	public EraseJunkMail() 
+	{
+		eraser = (JunkMailEraser)new InitialContext().lookup("java:global/hope_website/JunkMailEraser");
+	}
+	
+	
     @SuppressWarnings("unused")
     @OnEvent(component="eraseJunkMail", value="clicked")
     private void eraseJunkMail()
-    {
+    {    	
+    	eraser.erase();
         junkMailErased = "Erased!";
     }
     
@@ -25,8 +38,22 @@ public class EraseJunkMail
     		junkMailErased = null;
     	}
     }
+    
+    
+    public String getJunkMailErased()
+    {
+    	if(junkMailErased == null)
+    	{
+    		return "<None>";
+    	}
+    	else
+    	{
+    		return junkMailErased;
+    	}
+    }
 
     
     @Persist("flash")
-    @Getter private String junkMailErased;
-}
+    private String junkMailErased;    
+    private final JunkMailEraser eraser;
+}    
