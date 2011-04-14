1c8bb0a2a83a40c7f52ed0a7df888264629674b4
lprimak <lprimak@localhost>
2011-04-13 22:00:35 -0400

    cleaned up / refactored page lifecycle

--- src/java/com/flowlogix/website/pages/EraseJunkMail.java
+++ src/java/com/flowlogix/website/pages/EraseJunkMail.java
@@ -4,12 +4,13 @@ import javax.ejb.EJBException;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
+import lombok.Getter;
 import lombok.SneakyThrows;
 
 import org.apache.tapestry5.annotations.OnEvent;
-import org.apache.tapestry5.annotations.PageReset;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Secure;
+import org.apache.tapestry5.annotations.SetupRender;
 
 import com.flowlogix.website.JunkMailEraser;
 
@@ -34,6 +35,16 @@ public class EraseJunkMail
 		this.eraser = eraser;
 	}
 	
+
+    @SetupRender
+    void init()
+    {
+        if(junkMailErased == null)
+        {
+            junkMailErased = "<None>";
+        }
+    }
+	
 	
     @SuppressWarnings("unused")
     @OnEvent(component="eraseJunkMail", value="clicked")
@@ -49,33 +60,9 @@ public class EraseJunkMail
     		junkMailErased = "Erased!";
     	}
     }
-    
-    
-    @SuppressWarnings("unused")
-	@PageReset
-    private void pageReset()
-    {
-    	if(junkMailErased != null)
-    	{
-    		junkMailErased = null;
-    	}
-    }
-    
-    
-    public String getJunkMailErased()
-    {
-    	if(junkMailErased == null)
-    	{
-    		return "<None>";
-    	}
-    	else
-    	{
-    		return junkMailErased;
-    	}
-    }
-
+        
     
     @Persist("flash")
-    private String junkMailErased;    
+    @Getter private String junkMailErased;    
     private final JunkMailEraser eraser;
 }    
