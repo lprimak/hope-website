46d8715971149435aae38730dc37e7be1caf72a5
lprimak <lprimak@localhost>
2011-04-17 15:02:21 -0400

    factored out highlighter into a separate script file

--- src/java/com/flowlogix/website/mixins/ColorHighlight.java
+++ src/java/com/flowlogix/website/mixins/ColorHighlight.java
@@ -1,25 +1,49 @@
 package com.flowlogix.website.mixins;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import javax.validation.constraints.NotNull;
 
+import lombok.Cleanup;
+import org.apache.tapestry5.Asset;
 import org.apache.tapestry5.BindingConstants;
 import org.apache.tapestry5.annotations.Environmental;
 import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Path;
 import org.apache.tapestry5.annotations.SetupRender;
+import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.javascript.JavaScriptSupport;
 
 public class ColorHighlight
 {
-    @SetupRender
-    void init()
+    public ColorHighlight() throws IOException
     {
-        String script = "Tapestry.ElementEffect.colorhighlight = function(element) {" +
-        "return new Effect.Highlight(element, { startcolor : '%s' });" +
-        "}";
+        @Cleanup InputStream strm = scriptAsset.getResource().openStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(strm));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+
+        while ((line = br.readLine()) != null) {
+            sb.append(line + "\n");
+        }
+
+        script = sb.toString();
+    }
+    
+    
+    @SetupRender
+    void init() throws IOException
+    {
         js.addScript(script, color);
     }
+    
     
     @Environmental private JavaScriptSupport js;
     @Parameter(required=true, defaultPrefix=BindingConstants.LITERAL) @NotNull 
     private String color;
+    @Inject @Path("context:scripts/ColorHighlight.js")
+    private Asset scriptAsset;
+    private final String script;
 }
