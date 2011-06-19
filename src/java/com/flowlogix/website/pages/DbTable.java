35f8c480734f9661bcd524dd53f05048b68e9014
lprimak <lprimak@localhost>
2011-06-19 16:33:02 -0500

    tynamo security / jpa integration

---
+++ src/java/com/flowlogix/website/pages/DbTable.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.flowlogix.website.pages;
+
+/**
+ *
+ * @author lprimak
+ */
+public class DbTable
+{
+    
+}
--- src/java/com/flowlogix/website/services/HopeModule.java
+++ src/java/com/flowlogix/website/services/HopeModule.java
@@ -1,26 +1,37 @@
 package com.flowlogix.website.services;
 
+import org.apache.shiro.realm.Realm;
 import org.apache.tapestry5.SymbolConstants;
+import org.apache.tapestry5.ioc.Configuration;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.ServiceBinder;
 import org.apache.tapestry5.ioc.annotations.SubModule;
+import org.tynamo.shiro.extension.realm.text.ExtendedPropertiesRealm;
 
 @SubModule(EjbModule.class)
-public class HopeModule 
+public class HopeModule
 {
     public static void bind(ServiceBinder binder)
     {
         // binder.bind(MyServiceInterface.class, MyServiceImpl.class);
-        
+
         // Make bind() calls on the binder object to define most IoC services.
         // Use service builder methods (example below) when the implementation
         // is provided inline, or requires more initialization than simply
         // invoking the constructor.
     }
+
     
-	public static void contributeApplicationDefaults(MappedConfiguration<String, String> configuration)
-	{
-		configuration.add(SymbolConstants.PRODUCTION_MODE, "false");
-		configuration.add(SymbolConstants.SECURE_ENABLED, System.getProperty("hope.secure_enabled", "false"));
-	}
+    public static void contributeApplicationDefaults(MappedConfiguration<String, String> configuration)
+    {
+        configuration.add(SymbolConstants.PRODUCTION_MODE, "false");
+        configuration.add(SymbolConstants.SECURE_ENABLED, System.getProperty("hope.secure_enabled", "false"));
+    }
+    
+    
+    public static void contributeWebSecurityManager(Configuration<Realm> configuration)
+    {
+        ExtendedPropertiesRealm realm = new ExtendedPropertiesRealm("classpath:shiro-users.properties");
+        configuration.add(realm);
+    }
 }
