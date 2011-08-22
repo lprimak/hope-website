6ab890649388fe67f15109e6988b0fe3809c2d5f
lprimak <lprimak@localhost>
2011-08-22 19:09:04 -0400

    mock doesn't need to refer to the bean interface

--- src/main/java/com/flowlogix/website/impl/JunkMailEraserMock.java
+++ src/main/java/com/flowlogix/website/impl/JunkMailEraserMock.java
@@ -3,11 +3,9 @@ package com.flowlogix.website.impl;
 import javax.ejb.Stateless;
 
 import com.flowlogix.website.JunkMailEraserLocal;
-import javax.ejb.Local;
 
 
 @Stateless
-@Local(JunkMailEraserLocal.class)
 public class JunkMailEraserMock implements JunkMailEraserLocal
 {
     @Override
