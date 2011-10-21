8bf4db0da47acaf97ff364482c8b10a1df200c48
lprimak <lprimak@localhost>
2011-10-21 19:12:55 -0400

    added ajax / zone for date testing

--- src/main/java/com/flowlogix/website/pages/TestDateField.java
+++ src/main/java/com/flowlogix/website/pages/TestDateField.java
@@ -5,6 +5,7 @@
 package com.flowlogix.website.pages;
 
 import com.flowlogix.web.mixins.ColorHighlightOverride;
+import com.flowlogix.web.services.annotations.AJAX;
 import com.flowlogix.website.entities.Sample;
 import java.util.Date;
 import java.util.List;
@@ -12,10 +13,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.Setter;
 import org.apache.tapestry5.BindingConstants;
+import org.apache.tapestry5.Block;
+import org.apache.tapestry5.annotations.InjectComponent;
 import org.apache.tapestry5.annotations.Mixin;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.SetupRender;
+import org.apache.tapestry5.corelib.components.Zone;
 
 /**
  *
@@ -31,8 +36,16 @@ public class TestDateField
     }
     
     
+    @AJAX(requireSession = false)
+    Block onSubmitFromForm()
+    {
+        return birthdayZone.getBody();
+    }
+    
+    
     private @Getter List<Sample> birthdays;
-    private @Getter Date birthday;
+    private @Getter @Setter Date birthday;
+    private @InjectComponent Zone birthdayZone;
     private @PersistenceContext(unitName = "Hope") EntityManager em;
 
     @Parameter(defaultPrefix = BindingConstants.LITERAL, value = "#5AACFD") @NotNull private String highlightColor;
