f628a77e0052f0b7b17d37b4fbffe4159290c2ac
lprimak <lprimak@localhost>
2011-10-21 20:22:00 -0400

    streamlined, added beaneditform within the zone

--- src/main/java/com/flowlogix/website/pages/TestDateField.java
+++ src/main/java/com/flowlogix/website/pages/TestDateField.java
@@ -7,7 +7,6 @@ package com.flowlogix.website.pages;
 import com.flowlogix.web.mixins.ColorHighlightOverride;
 import com.flowlogix.web.services.annotations.AJAX;
 import com.flowlogix.website.entities.Sample;
-import java.util.Date;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -19,6 +18,7 @@ import org.apache.tapestry5.Block;
 import org.apache.tapestry5.annotations.InjectComponent;
 import org.apache.tapestry5.annotations.Mixin;
 import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.SetupRender;
 import org.apache.tapestry5.corelib.components.Zone;
 
@@ -32,19 +32,17 @@ public class TestDateField
     private void populate()
     {
         birthdays = em.createNamedQuery("Sample.findAll", Sample.class).getResultList();
-        birthday = birthdays.get(0).getDoB();
     }
     
     
     @AJAX(requireSession = false)
-    Block onSubmitFromForm()
+    Block onSubmit()
     {
         return birthdayZone.getBody();
     }
     
     
-    private @Getter List<Sample> birthdays;
-    private @Getter @Setter Date birthday;
+    private @Persist @Getter List<Sample> birthdays;
     private @InjectComponent Zone birthdayZone;
     private @PersistenceContext(unitName = "Hope") EntityManager em;
 
