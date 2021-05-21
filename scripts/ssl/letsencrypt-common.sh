7168d12c5efbc82d040b1a2591b08bf2cb7e050b
lprimak <lenny@flowlogix.com>
2021-05-21 02:26:52 -0500

    hide email

--- scripts/ssl/letsencrypt-common.sh
+++ scripts/ssl/letsencrypt-common.sh
@@ -2,7 +2,7 @@
 
 letsencrypt_dir=$HOME/var/letsencrypt
 
-certbot "$@" -n --webroot --agree-tos --email lprimak@hope.nyc.ny.us \
+certbot "$@" -n --webroot --agree-tos --email admin@flowlogix.com \
 --webroot-path ~website/contents \
 --logs-dir $HOME/var/log/letsencrypt --config-dir $letsencrypt_dir \
 --work-dir $HOME/var/lib
