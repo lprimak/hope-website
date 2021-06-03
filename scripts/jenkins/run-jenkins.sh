fa72d8a5756162fce409af535e787b9aec4c5a6d
FlowLogix Jenkins <lenny@flowlogix.com>
2021-06-03 12:47:06 -0500

    artifact permissions check

--- scripts/jenkins/run-jenkins.sh
+++ scripts/jenkins/run-jenkins.sh
@@ -4,7 +4,9 @@ SCRIPT_DIR=`dirname "$0"`
 
 jenkins_bin=$HOME/apps/jenkins/current
 
-exec java -Xmx256m -DJENKINS_HOME=$HOME/var/jenkins -jar $jenkins_bin/jenkins.war \
+exec java -Xmx256m -DJENKINS_HOME=$HOME/var/jenkins \
+-Dhudson.security.ArtifactsPermission=true \
+-jar $jenkins_bin/jenkins.war \
 --httpPort=8088 --httpsPort=-1 --http2Port=-1 \
 --extraLibFolder=$jenkins_bin "$@"
 
