ff528062947da5ed703d4fc0bf27a0bb77310eda
lprimak <lenny@flowlogix.com>
2022-01-20 00:17:22 -0600

    update jenkins jdk

--- scripts/jenkins/run-jenkins.sh
+++ scripts/jenkins/run-jenkins.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=`dirname "$0"`
 
 jenkins_bin=$HOME/apps/jenkins/current
 
-exec `sdk h java 11.0.13-zulu`/bin/java -Xmx256m -DJENKINS_HOME=$HOME/var/jenkins \
+exec `sdk h java 11.0.14-zulu`/bin/java -Xmx256m -DJENKINS_HOME=$HOME/var/jenkins \
 -Dhudson.security.ArtifactsPermission=true \
 -jar $jenkins_bin/jenkins.war \
 --httpPort=8088 --httpsPort=-1 --http2Port=-1 \
