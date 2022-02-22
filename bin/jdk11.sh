d1b7f46c3f953e1316b430334e973f7bd40f2c09
FlowLogix Jenkins <lenny@flowlogix.com>
2022-02-21 21:29:05 -0600

    update everything to jdk 17

--- bin/jdk11.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -pl
-
-sdk use java 11.0.14-zulu >/dev/null 2>&1
-exec "$@"
--- scripts/jenkins/run-jenkins.sh
+++ scripts/jenkins/run-jenkins.sh
@@ -4,9 +4,12 @@ SCRIPT_DIR=`dirname "$0"`
 
 jenkins_bin=$HOME/apps/jenkins/current
 
-exec `sdk h java 11.0.14-zulu`/bin/java -Xmx256m -DJENKINS_HOME=$HOME/var/jenkins \
+exec java -Xmx256m \
+--add-opens java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED \
+--add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.util.concurrent=ALL-UNNAMED \
+-DJENKINS_HOME=$HOME/var/jenkins \
 -Dhudson.security.ArtifactsPermission=true \
--jar $jenkins_bin/jenkins.war \
+-jar $jenkins_bin/jenkins.war --enable-future-java \
 --httpPort=8088 --httpsPort=-1 --http2Port=-1 \
 --extraLibFolder=$jenkins_bin "$@"
 
